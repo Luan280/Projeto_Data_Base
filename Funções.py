80a266c894cf068383eee2f5636056bfa166526f
Luan <luanferreiralima280@gmail.com>
2024-08-30 11:34:11 -0300

    Arquivo txt e texto de usuários cadastrados.

--- Funções.py
+++ Funções.py
@@ -16,11 +16,11 @@ def cadastrar(nome, email):
     confirmacao = True
     for pessoa in resultado:
         if pessoa[2] == email:
-            print("EMAIL JÁ CADASTRADO")
             confirmacao = False
     if confirmacao:
         cursor.execute(f"INSERT INTO pessoas (nome, email) VALUES ('{nome}', '{email}')")
-        print("EMAIL CADASTRADO COM SUCESSO!")
+        with open("arquivo.txt", "a") as arquivo:
+            arquivo.write(f"{nome}, {email}\n")
     conexao.commit()
     cursor.close()
     conexao.close()
@@ -29,6 +29,7 @@ def cadastrar(nome, email):
 
 
 def remover(email):
+    global confirmacao
     conexao = mysql.connector.connect(
         host='localhost',
         user='root',
@@ -64,5 +65,9 @@ def consultar():
     )
     cursor = conexao.cursor()
     cursor.execute(f"SELECT * FROM pessoas")
+    conexao.commit()
     resultado = cursor.fetchall()
-    return resultado
+    with open("arquivo.txt", "r") as arquivo:
+        conteudo = arquivo.read()
+    msg_consulta = conteudo
+    return msg_consulta
--- Interface.py
+++ Interface.py
@@ -1,56 +1,68 @@
 import customtkinter
 from Funções import *
 
-
 def entrada_cadastro():
     confirmacao = cadastrar(entrada_nome.get(), entrada_email.get())
     if confirmacao:
-        msg_confirmacao.configure(text="EMAIl CADASTRADO COM SUCESSO!")
+        texto_confirmacao.configure(text="EMAIl CADASTRADO COM SUCESSO!")
     else:
-        msg_confirmacao.configure(text="EMAIL JÁ UTILIZADO")
+        texto_confirmacao.configure(text="EMAIL JÁ UTILIZADO")
     entrada_nome.delete(0, "end")
     entrada_email.delete(0, "end")
 
 def entrada_deletar():
     confirmacao = remover(entrada_email.get())
     if confirmacao:
-        msg_confirmacao.configure(text="USUÁRIO REMOVIDO COM SUCESSO!")
+        texto_confirmacao.configure(text="USUÁRIO REMOVIDO COM SUCESSO!")
     else:
-        msg_confirmacao.configure(text="USUÁRIO NÃO CADASTRADO")
+        texto_confirmacao.configure(text="USUÁRIO NÃO CADASTRADO")
     entrada_nome.delete(0, "end")
     entrada_email.delete(0, "end")
 
 
-def janela_consultar():
-    texto = consultar()
-    for pessoa in texto:
-        print(f"ID:{pessoa[0]} NOME: {pessoa[1]} EMAIL: {pessoa[2]}")
-    frame_consulta = customtkinter.CTkFrame(janela)
-    frame_consulta.pack()
-    texto_consulta = customtkinter.CTkLabel(frame_consulta, text=texto)
-    texto_consulta.pack()
+def entrada_consultar():
+    with open("arquivo.txt", "r") as arquivo:
+        conteudo = arquivo.read()
+    texto_consulta.configure(text=conteudo)
 
-
+# Configuração da interface
 janela = customtkinter.CTk()
 janela.geometry("900x900")
 customtkinter.set_default_color_theme("dark-blue")
 
+msg_consulta=""
 resposta = ""
+
+# Caixa de entrada do Nome
 entrada_nome = customtkinter.CTkEntry(janela, placeholder_text="NOME", width=600, height=40)
 entrada_nome.pack()
 
+# Caixa de entrada do Email
 entrada_email = customtkinter.CTkEntry(janela, placeholder_text="E-MAIL", width=600, height=40)
 entrada_email.pack()
 
+# Botão que cadastra o usuário
 botao_cadastra = customtkinter.CTkButton(janela, text="CADASTRAR", command=entrada_cadastro)
 botao_cadastra.pack()
 
+# Botão que deleta um usuário
 botao_deletar = customtkinter.CTkButton(janela, text="DELETAR", command=entrada_deletar)
 botao_deletar.pack()
 
-botao_consultar = customtkinter.CTkButton(janela, text="CONSULTAR", command=janela_consultar)
+# Botão que consulta todos os usuário
+botao_consultar = customtkinter.CTkButton(janela, text="CONSULTAR", command=entrada_consultar)
 botao_consultar.pack()
 
-msg_confirmacao = customtkinter.CTkLabel(janela, text=resposta)
-msg_confirmacao.pack()
+# Texto que valida se o usuário foi cadastrado, se o email já existe e se foi deletado com sucesso
+texto_confirmacao = customtkinter.CTkLabel(janela, text=resposta)
+texto_confirmacao.pack()
+
+# Frame que mostra todos os usuários
+frame_consulta = customtkinter.CTkFrame(janela)
+frame_consulta.pack()
+
+# Texto que mostra todas os usuários cadastrados
+texto_consulta = customtkinter.CTkLabel(frame_consulta, text=msg_consulta)
+texto_consulta.pack()
+
 janela.mainloop()
