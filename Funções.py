832e18aeeca3fac1e2303f221af7e01f2926ad35
Luan <luanferreiralima280@gmail.com>
2024-09-07 00:44:29 -0300

    Interface e funcionalidade atualizar.

--- Funções.py
+++ Funções.py
@@ -27,7 +27,6 @@ def cadastrar(nome, email):
     return encontrado
 
 
-
 def remover(email):
     conexao = mysql.connector.connect(
         host='localhost',
@@ -39,12 +38,10 @@ def remover(email):
     cursor.execute(f"SELECT * FROM pessoas")
     resultado = cursor.fetchall()
     conexao.commit()
-    print(resultado)
+    encontrado = True
     for pessoa in resultado:
         if email == pessoa[2]:
             cursor.execute(f"DELETE FROM pessoas WHERE email = '{email}'")
-
-            encontrado = True
             conexao.commit()
             cursor.close()
             conexao.close()
@@ -66,14 +63,41 @@ def consultar():
     )
     cursor = conexao.cursor()
     cursor.execute(f"SELECT * FROM pessoas")
-    resultado = cursor.fetchall()
+    banco_dados = cursor.fetchall()
     conexao.commit()
     with open("arquivo.txt", "w"):
         pass
-    for usuario in resultado:
+    for usuario in banco_dados:
         with open("arquivo.txt", "a") as arquivo:
             arquivo.write(f"ID: {usuario[0]}  NOME: {usuario[1]}  EMAIL: {usuario[2]}\n")
             arquivo.write(' \n')
     with open("arquivo.txt", "r") as arquivo:
         conteudo = arquivo.read()
     return conteudo
+
+
+def atualizar(id_usuario, nome, email):
+    conexao = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='@Luan280',
+        database='projeto_db'
+    )
+    cursor = conexao.cursor()
+    confirmacao = True
+    if id_usuario in "":
+        pass
+        # VER se o ID já foi registrado.
+    elif nome not in "":
+        cursor.execute(f"UPDATE pessoas SET email = '{nome}' WHERE id = '{id_usuario}'")
+    elif email not in "":
+        cursor.execute(f"UPDATE pessoas SET nome = '{email}' WHERE id = '{id_usuario}'")
+    elif email not in "" and nome not in "":
+        cursor.execute(f"UPDATE pessoas SET nome = '{nome}' WHERE id = '{id_usuario}'")
+        cursor.execute(f"UPDATE pessoas SET email = '{email}' WHERE id = '{id_usuario}'")
+    elif email in "" and nome in "" and id_usuario in "":
+        confirmacao = False
+    conexao.commit()
+    cursor.close()
+    conexao.close()
+    return confirmacao
--- Interface.py
+++ Interface.py
@@ -8,8 +8,12 @@ def entrada_cadastro():
         texto_confirmacao.configure(text="EMAIl CADASTRADO COM SUCESSO!")
     else:
         texto_confirmacao.configure(text="EMAIL JÁ UTILIZADO")
+    # Deleta o texto que está na entrada de dados
     entrada_nome.delete(0, "end")
-    entrada_email.delete(0, "end")
+    entrada_email.delete(0, 'end')
+    # Escreve um texto invisível na caixa de entrada de dados
+    entrada_nome.configure(placeholder_text="NOME")
+    entrada_email.configure(placeholder_text="E-MAIL")
 
 def entrada_deletar():
     confirmacao = remover(entrada_email.get())
@@ -17,8 +21,12 @@ def entrada_deletar():
         texto_confirmacao.configure(text="USUÁRIO REMOVIDO COM SUCESSO!")
     else:
         texto_confirmacao.configure(text="USUÁRIO NÃO CADASTRADO")
+    # Deleta o texto que está na entrada de dados
     entrada_nome.delete(0, "end")
     entrada_email.delete(0, "end")
+    # Escreve um texto invisível na caixa de entrada de dados
+    entrada_nome.configure(placeholder_text="NOME")
+    entrada_email.configure(placeholder_text="E-MAIL")
 
 
 def entrada_consultar():
@@ -26,8 +34,30 @@ def entrada_consultar():
     texto_consulta.configure(text=conteudo)
     frame_consulta.place(x=90, y=20)
 
+def entrada_frame_atualizar():
+    frame_atualizar.place(x=50, y=25)
+
+
+def entrada_atualizar():
+    confirmacao = atualizar(id_atualizar.get(), nome_atualizar.get(), email_atualizar.get())
+    if confirmacao:
+        texto_confirmacao_atualizar.configure(text="DADOS ATUALIZADOS COM SUCESSO.")
+    else:
+        texto_confirmacao_atualizar.configure(text="ID DE USUÁRIO NÃO IDENTIFICADO. ")
+        # Deleta o texto que está na entrada de dados
+        id_atualizar.delete(0, "end")
+        nome_atualizar.delete(0, "end")
+        email_atualizar.delete(0, "end")
+        # Escreve um texto invisível na caixa de entrada de dados
+    id_atualizar.configure(placeholder_text="ID")
+    nome_atualizar.configure(placeholder_text="NOME")
+    email_atualizar.configure(placeholder_text="E-MAIL")
 def fechar_frame():
     frame_consulta.place(x=2000, y=2000)
+
+
+def fechar_frame_atualizar():
+    frame_atualizar.place(x=2000, y=2000)
 
 # Configuração da interface
 janela = customtkinter.CTk()
@@ -42,8 +72,15 @@ msg_consulta = " "
 icone_usuario = customtkinter.CTkImage(dark_image=Image.open("imagens/icon_user.png"), size=(35,35))
 icone_email = customtkinter.CTkImage(dark_image=Image.open("imagens/icon_email.webp"), size=(40,40))
 icone_fechar = customtkinter.CTkImage(dark_image=Image.open("imagens/x_fechar.png"))
+
 frame_principal = customtkinter.CTkFrame(janela,fg_color="white",width=1055, height=1010)
 frame_principal.place(x=800,y=1)
+
+# Frame que mostra todos os usuários
+frame_consulta = customtkinter.CTkScrollableFrame(janela, fg_color="#AFBF9F", width=550, height=850, corner_radius=50)
+
+# Frame que atualiza os dados dos usuários
+frame_atualizar = customtkinter.CTkFrame(janela, fg_color="#AFBF9F", width=700, height=950, corner_radius=50)
 
 # Caixa de entrada do Nome
 entrada_nome = customtkinter.CTkEntry(frame_principal, placeholder_text="NOME", width=600, height=40,corner_radius=10,
@@ -61,6 +98,18 @@ entrada_email.place(x=220 ,y=550)
 imagem_email = customtkinter.CTkLabel(frame_principal, image=icone_email, text="")
 imagem_email.place(x=230 ,y=505)
 
+id_atualizar = customtkinter.CTkEntry(frame_atualizar, placeholder_text="ID", width=500, height=40,corner_radius=10,
+                                      border_color="#67735C", fg_color="#D9D9D9", text_color="#262626")
+id_atualizar.place(x=100,y=300)
+
+nome_atualizar = customtkinter.CTkEntry(frame_atualizar, placeholder_text="NOME", width=500, height=40,corner_radius=10,
+                                      border_color="#67735C", fg_color="#D9D9D9", text_color="#262626")
+nome_atualizar.place(x=100,y=400)
+
+email_atualizar = customtkinter.CTkEntry(frame_atualizar, placeholder_text="EMAIL", width=500, height=40,corner_radius=10,
+                                      border_color="#67735C", fg_color="#D9D9D9", text_color="#262626")
+email_atualizar.place(x=100,y=500)
+
 # Botão que cadastra o usuário
 botao_cadastra = customtkinter.CTkButton(frame_principal, text="CADASTRAR", command=entrada_cadastro,
                                          width=320,height=60, corner_radius=20,
@@ -74,15 +123,29 @@ botao_consultar = customtkinter.CTkButton(frame_principal, text="CONSULTAR", com
 botao_consultar.place(x=350,y=700)
 
 # Atualiza os dados do usuário
-botao_atualizar = customtkinter.CTkButton(frame_principal, text="ATUALIZAR", command=entrada_consultar, width=320,
-                                          height=60, corner_radius=20,fg_color="#AFBF9F",
-                                          hover_color="#E9F2C9",text_color="#262626")
+botao_atualizar = customtkinter.CTkButton(frame_principal, text="ATUALIZAR", command=entrada_frame_atualizar, width=320,
+                                          height=60, corner_radius=20, fg_color="#AFBF9F",
+                                          hover_color="#E9F2C9", text_color="#262626")
 botao_atualizar.place(x=350,y=780)
 
 # Botão que deleta um usuário
 botao_deletar = customtkinter.CTkButton(frame_principal, text="DELETAR", command=entrada_deletar, width=320,height=60,
                                         corner_radius=20,fg_color="#AFBF9F", hover_color="#E9F2C9",text_color="#262626")
 botao_deletar.place(x=350,y=860)
+
+# Botão que fecha o frame de consulta
+botao_fechar_consulta = customtkinter.CTkButton(frame_consulta, image=icone_fechar, command=fechar_frame,
+                        text="", width=5, height=5,bg_color="transparent", fg_color="transparent",hover_color= 'grey')
+botao_fechar_consulta.place(x=450, y=0)
+
+# Botão que fecha o frame que atualiza dados
+botao_fechar_atualizar = customtkinter.CTkButton(frame_atualizar, image=icone_fechar, command=fechar_frame_atualizar, text="",
+                                width=5, height=5,bg_color="transparent", fg_color="transparent", hover_color= 'grey')
+botao_fechar_atualizar.place(x=450, y=0)
+
+botao_frame_atualizar = customtkinter.CTkButton(frame_atualizar, text="ATUALIZAR", command=entrada_atualizar, width=320,
+                                                height=60, corner_radius=20, fg_color="#AFBF9F", hover_color="#E9F2C9", text_color="#262626")
+botao_frame_atualizar.place(x=150, y=600)
 
 texto_principal = customtkinter.CTkLabel(frame_principal, text="CRUD", text_color="#67735C", font=("arial", 100))
 texto_principal.place(x=370 ,y=100)
@@ -97,15 +160,11 @@ texto_usuario.place(x=280 ,y=350)
 texto_email = customtkinter.CTkLabel(frame_principal, text="E-MAIL:", text_color="#67735C", font=("arial", 40))
 texto_email.place(x=280 ,y=500)
 
-# Frame que mostra todos os usuários
-frame_consulta = customtkinter.CTkScrollableFrame(janela, fg_color="#AFBF9F", width=550, height=850, corner_radius=50)
-
 # Texto dos usuários
 texto_consulta = customtkinter.CTkLabel(frame_consulta, text=msg_consulta, text_color="white", font=("arial", 15))
 texto_consulta.pack()
 
-# Botão que fecha o frame de consulta
-bota_fechar = customtkinter.CTkButton(frame_consulta, image=icone_fechar, command=fechar_frame, text="", width=5, height=5,
-                            bg_color="transparent", fg_color="transparent",hover_color= 'grey')
-bota_fechar.place(x=450, y=0)
+texto_confirmacao_atualizar = customtkinter.CTkLabel(frame_atualizar, text=resposta, font=('arial', 30), text_color="#262626")
+texto_confirmacao_atualizar.place(x=100 ,y=100)
+
 janela.mainloop()
